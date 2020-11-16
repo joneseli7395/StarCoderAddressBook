b3cefca49e62a3b906b6ed8f27977c8534e7c56e
Eli Jones <joneseli7395@gmail.com>
2020-11-16 09:55:33 -0500

    Initial commit

---
+++ Data/Migrations/20201116020157_Initial001.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StarCoderAddressBook.Data.Migrations
+{
+    public partial class Initial001 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contact",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Address1 = table.Column<string>(nullable: true),
+                    Address2 = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    State = table.Column<string>(nullable: true),
+                    ZipCode = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true),
+                    DateAdded = table.Column<string>(nullable: true),
+                    FilePath = table.Column<string>(nullable: true),
+                    FileData = table.Column<byte[]>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contact", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contact");
+        }
+    }
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarCoderAddressBook.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        [Display(Name = "Address 1")]
+        public string Address1 { get; set; }
+        [Display(Name = "Address 2")]
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string Phone { get; set; }
+        [Display(Name = "Date Added")]
+        public string DateAdded { get; set; }
+        public string FilePath { get; set; }
+        public byte[] FileData { get; set; }
+    }
+}
